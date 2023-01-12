30fbe33edce699c42cab1327e0784e1d02e65860
Apethesis <apethesis@gmail.com>
2023-01-12 04:12:36 +0100

    Added combtAPI2 to packages.
    Co-authored-by: Levi Miller <ldextermiller@gmail.com>

---
+++ combtAPI2.lua
@@ -0,0 +1,19 @@
+local package = {
+    unicornSpec = "v1.0.0",
+    name = "combtAPI2",
+    desc = "Easy to use button api.",
+    dirs = {},
+    maintainer = "CCpeg",
+    licensing = "MIT",
+    version = "1.0.0",
+    pkgType = "com.github",
+    instdat = {
+        repo_owner = "Apethesis",
+        repo_name = "CC-Code",
+        repo_ref = "master",
+        filemaps = {
+            ["combtAPI2.lua"] = "/lib/combtAPI2.lua"
+        }
+    }
+}
+return package
