7da0688f089af2c5c29bd6a1b73c0c7b76fa826a
Apethesis <apethesis@gmail.com>
2023-01-12 03:52:27 +0100

    Added cimg2 package (testing)
    Co-authored-by: Levi Miller <ldextermiller@gmail.com>

---
+++ cimg2.lua
@@ -0,0 +1,19 @@
+local package = {
+    unicornSpec = "v1.0.0",
+    name = "cimg2",
+    desc = "Easy to use library for interacting with .cimg2 files.",
+    dirs = {},  
+    maintainer = "CCpeg",
+    licensing = "MIT",
+    version = "1.0.0",
+    pkgType = "com.github",
+    instdat = {
+        repo_owner = "Apethesis",
+        repo_name = "CC-Code",
+        repo_ref = "master",
+        filemaps = {
+            "cimg2.lua" = "/lib/cimg2.lua"
+        }
+    }
+}
+return package
